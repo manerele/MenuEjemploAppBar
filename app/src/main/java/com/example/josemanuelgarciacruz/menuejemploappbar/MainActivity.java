c7697f7180c71061da96d36e4daceba9ec4c7894
Manuel <manerele@telefonica.net>
2018-10-08 10:39:45 +0100

    Al pinchar en un lanzador nos va a otra actividad min 19:52

--- app/src/main/java/com/example/josemanuelgarciacruz/menuejemploappbar/MainActivity.java
+++ app/src/main/java/com/example/josemanuelgarciacruz/menuejemploappbar/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.josemanuelgarciacruz.menuejemploappbar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_lanador:
+                Intent intent = new Intent(getApplicationContext(), SeccionesActivity.class);
+                startActivity(intent);
+                break;
+
+            default:
+                break;
+        }
         return true;
     }
 }
